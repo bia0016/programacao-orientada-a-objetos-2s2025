6014bd457941b61575570011ec52a298956b5565
bia0016 <biaevangelistamoreira@gmail.com>
2025-08-27 11:23:18 -0300

    novo

--- olamundo.java
+++ olamundo.java
@@ -1,5 +1,5 @@
 import javax.swing.JOptionPane;
-class olamundo {
+class OlaMundo {
 	public static void main(String[] args){
 		System.out.println("Olá, mundo!");
 		JOptionPane.showMessageDialog(null, "Olá, mundo!");
